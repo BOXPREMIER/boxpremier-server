65c2afa3dc1d6c13ff737bbfcc2987483134b315
priscelis <prisceliscodrington@gmail.com>
2025-10-27 10:31:42 +0100

    feat(subscriptions): finalize SubscriptionPlan model with schema (#67)

---
+++ src/models/SubscriptionPlan.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+
+const SubscriptionPlanSchema = new Schema(
+    
+    {
+     boxType: {
+        type: String,
+        enum: ["basic", "premium"], 
+        required: true  
+     },
+     boxSize: {
+        type: Number, 
+        required: true
+     },
+     price: {
+        type: Number, 
+        required: true
+     },
+     active:{
+        type: Boolean,
+        required: true
+     },
+
+        //soft-delete field
+    deleteAt: { type: Date, default: null }
+    
+    }, 
+    {
+        timestamps: true, 
+    });
+
+userSchema.pre(/^find/, function (next) {
+    this.where({ deleteAt: null });
+    next();
+});
+
+//method to soft-delete
+userSchema.methods.softDelete = function () {
+    this.deleteAt = new Date();
+    return this.save();
+}
+
+export default model("SubscriptionPlan", SubscriptionPlanSchema);
