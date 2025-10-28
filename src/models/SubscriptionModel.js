906294a53af98ccc1bae7d67474e0debd54bfc75
priscelis <prisceliscodrington@gmail.com>
2025-10-28 22:17:30 +0100

    refactor: Changing the file name (#78)

---
+++ src/models/SubscriptionModel.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+
+const { Schema, model, Types } = mongoose;
+
+function isGiftRequired() {
+  return this.isGift === true;
+}
+
+const SubscriptionSchema = new Schema(
+  {
+
+    user: {
+      type: Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+
+    subscriptionPlan: {
+      type: Types.ObjectId,
+      ref: "SubscriptionPlan",
+      required: true,
+      index: true,
+    },
+
+    wineType: { type: String, enum: ["mixed", "rose", "red", "sparkling"], trim: true },
+    boxType: { type: String, enum: ["basic", "premium"], trim: true },
+    boxSize: { type: Number, min: 1 },
+
+    // Dates
+    startDate: { type: Date },
+    nextPayDate: { type: Date },
+    endDate: { type: Date },
+
+    status: {
+      type: String,
+      enum: ["active", "paused", "canceled", "expired", "pending"],
+      default: "pending",
+      index: true,
+    },
+
+    // Gift
+    isGift: { type: Boolean, default: false },
+
+    // if isGift == true 
+    giftFromId: {
+      type: Types.ObjectId, ref: "User", required: isGiftRequired,
+    },
+
+    giftMessage:{type: String, trim: true,maxlength: 500 },
+
+    // Payment
+    payMethod: { type: String, required: true },
+  },
+  {
+    // it adds createdAt y updatedAt
+    timestamps: true,
+  }
+);
+
+SubscriptionSchema.index({ user: 1, status: 1 });
+SubscriptionSchema.index({ startDate: -1 });
+
+export default model("Subscription", SubscriptionSchema);
