6c0b71d4331115408dd05abb8f47e8f2379fbda7
marianyarj <marianyarj@gmail.com>
2025-10-30 18:54:02 +0100

    feat: add updates functions in OrderController #79

--- src/controllers/OrderController.js
+++ src/controllers/OrderController.js
@@ -15,7 +15,7 @@ export const getAllOrders = async (req, res) => {
 
             // customer user see only their orders
         } else {
-            orders = (await OrderModel.find({ userId: req.user._id }).populate('subscriptionId')).toSorted({ orderDate: -1 });
+            orders = (await OrderModel.find({ userId: req.user._id }).populate('subscriptionId')).sort({ orderDate: -1 });
         }
 
         return handleSuccess(res, orders);
@@ -103,6 +103,126 @@ export const createOrder = async (req, res) => {
         });
 
         return handleCreated(res, order, 'Order created');
+    } catch (error) {
+        return handleError(res, error);
+    }
+};
+
+//updates functions
+
+export const updateOrderStatus = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { status } = req.body;
+
+        const validStatuses = ['pending', 'preparing', 'shipped', 'delivered', 'cancelled'];
+
+        if (!validStatuses.includes(status)) {
+            return handleError(res, { message: `Invalid status. Valid options ${validStatuses.join(', ')}` });
+        }
+
+        const order = await OrderModel.findById(id);
+        if (!order) {
+            return handleNotFound(res, 'Order not found');
+        }
+
+        // block status change if delivered
+        if (order.status === "delivered") {
+            return handleForbidden(res, 'Cannot change status of delivered order');
+        }
+
+        if (status === 'shipped' && (!order.trackingNumber || !order.carrier)) {
+            return handleError(res, { message: 'Cannot set status to shipped without tracking number and carrier' });
+        }
+
+        if (status === 'shipped') {
+            order.shippedDate = new Date();
+        }
+        if (status === 'delivered') {
+            order.deliveredDate = new Date();
+        }
+
+        order.status = status;
+        await order.save();
+
+        return handleSuccess(res, order, 'Order status updated successfully');
+    } catch (error) {
+        return handleError(res, error);
+    }
+};
+
+export const updateOrderAddress = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { street, number, floor, postalCode, city, province, country } = req.body;
+
+        const order = await OrderModel.findById(id);
+        if (!order) {
+            return handleNotFound(res, 'Order not found');
+        }
+
+        if (order.status === "delivered") {
+            return handleForbidden(res, 'Cannot change address of delivered order');
+        }
+
+        if (street) { order.street = street };
+        if (number) { order.number = number };
+        if (floor !== undefined) { order.floor = floor };
+        if (postalCode) { order.postalCode = postalCode };
+        if (city) { order.city = city };
+        if (province) { order.province = province };
+        if (country) { order.country = country };
+
+        await order.save();
+
+        return handleSuccess(res, order, 'Order addres updated successfully');
+    } catch (error) {
+        return handleError(res, error);
+    }
+};
+
+export const updateOrderTracking = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { trackingNumber, carrier } = req.body;
+
+        const order = await OrderModel.findById(id);
+        if (!order) {
+            return handleNotFound(res, 'Order not found');
+        }
+
+        if (trackingNumber !== undefined) {
+            order.trackingNumber = trackingNumber;
+        }
+
+        if (carrier !== undefined) {
+            order.carrier = carrier;
+        }
+
+        await order.save();
+        return handleSuccess(res, order, 'Order tracking updated successfully');
+    } catch (error) {
+        return handleError(res, error);
+    }
+};
+
+export const cancelOrder = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const order = await OrderModel.findById(id);
+        if (!order) {
+            return handleNotFound(res, 'Order not found');
+        }
+
+        if (!['pending', 'preparing'].includes(order.status)) {
+            return handleForbidden(res, "Cannot cancel order with status: 'shipped', 'delivered'");
+        }
+
+        order.status = 'cancelled';
+        await order.save();
+
+        return handleSuccess(res, order, 'Order cancelled successfully');
     } catch (error) {
         return handleError(res, error);
     }
