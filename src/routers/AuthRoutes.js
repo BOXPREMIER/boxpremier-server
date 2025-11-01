07fe0c53799e4eedb82975c86253ffc8c3c98cce
marianyarj <marianyarj@gmail.com>
2025-11-01 05:18:15 +0100

    feat: add registerValidator and loginValidator for authentication routes #76 #16

--- src/routers/AuthRoutes.js
+++ src/routers/AuthRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { registerController, loginController } from '../controllers/AuthController.js';
+import { loginValidator, registerValidator } from '../validators/AuthValidator.js';
 
 const AuthRouter = express.Router();
 
-AuthRouter.post('/register', registerController);
-AuthRouter.post('/login', loginController);
+AuthRouter.post('/register', registerValidator, registerController);
+AuthRouter.post('/login', loginValidator, loginController);
 
 export default AuthRouter;
