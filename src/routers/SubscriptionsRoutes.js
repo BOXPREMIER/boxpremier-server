c51a96e765d5ff4d99eaf0fd61d07bd4278d8558
priscelis <prisceliscodrington@gmail.com>
2025-11-01 12:30:35 +0100

    SubscriptionsRoutes modified (#78)

--- src/routers/SubscriptionsRoutes.js
+++ src/routers/SubscriptionsRoutes.js
@@ -1,9 +1,16 @@
 import express from 'express';
-import { createSubscription, listSubscriptions } from '../controllers/SubscriptionController.js';
+import { createSubscription, getUserSubscriptions, cancelSubscription, updateSubscription, getSubscriptionById } from '../controllers/SubscriptionController.js';
+import { authenticate, checkRole } from '../middlewares/authMiddleware.js';
+import { createSubscriptionValidator, updateSubscriptionValidator } from '../validators/SubscriptionValidator.js';
 
 const SubsRouter = express.Router();
 
-//SubsRouter.post('/newsubsriber', createSubscription);
-SubsRouter.get('/', listSubscriptions);
+SubsRouter.use(authenticate);
+
+SubsRouter.post('/', createSubscriptionValidator, createSubscription);
+SubsRouter.get('/details/:id', getSubscriptionById);
+SubsRouter.get('/:userId', checkRole(['admin']), getUserSubscriptions);
+SubsRouter.put('/:id', updateSubscriptionValidator, updateSubscription);
+SubsRouter.delete('/:id', cancelSubscription);
 
 export default SubsRouter;
